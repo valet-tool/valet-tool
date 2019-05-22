f1cdb0c1a2b01239b1f946ffa21694ee25fcbc8e
rit-valet <services+valet@cioffitech.com>
2019-05-22 12:38:07 -0400

    fix cron.sh

--- cron.sh
+++ cron.sh
@@ -1,5 +1,6 @@
 killall PiMonitor
 sleep(5)
-mv output.log `date +%Y-%m-%d`-output.csv
+cd /home/pi/valet-tool
+mv output.csv `date +%Y-%m-%d`-output.csv
 sleep(5)
 /home/pi/PiPowerMeasure/PiMonitor | tee /home/pi/PiPowerMeasure/output.csv
