17053bdcf3c09d4e2a8a88575e8a18c2564115ad
rit-valet <services+valet@cioffitech.com>
2019-05-24 12:31:31 -0400

    changes to cron script

--- cron.sh
+++ cron.sh
@@ -3,4 +3,4 @@ sleep(5)
 cd /home/pi/valet-tool
 mv output.csv `date +%Y-%m-%d`-output.csv
 sleep(5)
-/home/pi/PiPowerMeasure/PiMonitor | tee /home/pi/PiPowerMeasure/output.csv
+/home/pi/valet-tool/PiMonitor | tee /home/pi/valet-tool/output.csv
