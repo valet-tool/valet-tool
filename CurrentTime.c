3fa1daacee3d7c7db2a51fb0c5be0fc4accbd109
Parth Sane <pvs3325@rit.edu>
2019-06-11 12:36:33 -0400

    Added fix for millisecond time glitch

--- CurrentTime.c
+++ CurrentTime.c
@@ -8,6 +8,8 @@ void get_current_date_time(char* current_date_time)
 	char timeInSecs[2];
 	char timeInMins[2];
 	char timeInHours[2];
+	char timeInMsecs[3];
+	int intTimeInMS = time_us.tv_usec/1000;
 	if(tm.tm_hour<10){
 		sprintf(timeInHours,"0%d",tm.tm_hour);
 	}
@@ -19,6 +21,17 @@ void get_current_date_time(char* current_date_time)
 	}else{
 		sprintf(timeInSecs,"%d",tm.tm_sec);
 	}
+	if(intTimeInMS<10){
+		sprintf(timeInMsecs,"00%d",intTimeInMS);
+	}
+	else if (intTimeInMS<100 && intTimeInMS>10){
+		sprintf(timeInMsecs,"0%d",intTimeInMS);
+	}
+	else
+	{
+		sprintf(timeInMsecs,"%d",intTimeInMS);
+	}
+	
 	if(tm.tm_min<10){
 		sprintf(timeInMins,"0%d",tm.tm_min);
 	}
@@ -30,7 +43,7 @@ void get_current_date_time(char* current_date_time)
 	sprintf(current_date_time, "%d-%d-%dT%s:%s:%s:%lu", 
 		 tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, 
 		 timeInHours, timeInMins, 
-		 timeInSecs, time_us.tv_usec/1000);
+		 timeInSecs, timeInMsecs);
 	
 }
 
