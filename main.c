63b2d7241932fa1a80f0a79d56c1294f74faa8bc
Parth Sane <pvs3325@rit.edu>
2019-06-12 18:43:30 -0400

    Add multi server support and tactic 3(find command) step

--- main.c
+++ main.c
@@ -86,6 +86,16 @@ void *printMsg(void *ptr)
 	int serverNumber = 0;
 	char current_date_time_thread[26];
 	uint32_t delay = 1000000;
+	char zipfilename[100]="'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'";
+	char debDir[100]= "'LibreOffice_6.2.3_Linux_x86_deb'";
+	char whatToFind[100] = "'README_en-US'";
+	char filenameArgument[1000]=" '-O' 'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'";
+	char wgetCommand[1000]="wget ";
+    char link[1000]="";
+	char unzipCommand[100]="tar 'xvf' ";
+	char zipCommand[100]="tar '-cvzf' ";
+	char rmCommand[100]="rm ";
+	char findCommand[100]="find './' '-name' ";
 	file_thread = fopen(filename, "r");
 	if (file_thread != NULL)
 	{
@@ -102,17 +112,56 @@ void *printMsg(void *ptr)
 	}
 
 	signal(SIGINT, sig_handler);
-	
+	strcat(findCommand,whatToFind);
+	strcat(rmCommand,zipfilename);
+	strcat(unzipCommand,zipfilename);
+	strcat(zipCommand,zipfilename);
+	strcat(zipCommand,debDir);
 	while (keep_running)
 	{
 		int i = 0;
-		for (i = 0; i <= 0; i++)
-		{
+		for (i = 0; i < 9; i++)
+		{	
+			switch (i)
+			{
+			case 0:
+				strcat(link,"'http://tdf.ufes.br/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 1:
+				strcat(link,"'http://tdf.mirror.rafal.ca/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 2:
+				strcat(link,"'https://espejito.fder.edu.uy/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 3:
+				strcat(link,"'https://mirror.aptus.co.tz/pub/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 4:
+				strcat(link,"'http://ftp.kaist.ac.kr/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 5:
+				strcat(link,"'https://libreoffice.mirror.garr.it/mirrors/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 6:
+				strcat(link,"'http://ftp.bme.hu/pub/mirrors/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 7:
+				strcat(link,"'https://mirrors.ucr.ac.cr/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			case 8:
+				strcat(link,"'https://ftp.nluug.nl/office/libreoffice/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				break;
+			default:
+				printf("Invalid link: Unkown Error Occured!");
+				exit(1);
+				break;
+			}
+			strcat(wgetCommand,strcat(link,filenameArgument));
 			//Download: tactic 1
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
 			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,1, i+1);
-			system("wget 'http://tdf.ufes.br/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz' '-O' 'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+			system(wgetCommand);
 			
 			//Placeholder Sleep Tactic 0
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
@@ -124,7 +173,7 @@ void *printMsg(void *ptr)
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
 			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,2, i+1);
-			system("tar 'xvf' 'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+			system(unzipCommand);
 
 			//Placeholder Sleep Tactic 0
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
@@ -132,12 +181,23 @@ void *printMsg(void *ptr)
 			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
 			usleep(delay);
 
-			//Bla Bla Tactic 3
+			//find command: Tactic 3
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
 			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,3, i+1);
-			sleep(1);
+			system(findCommand);
+
+			//Placeholder Sleep Tactic 0
+			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
+			get_current_date_time(current_date_time_thread);
+			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
 			usleep(delay);
+
+			//compress command: Tactic 4
+			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
+			get_current_date_time(current_date_time_thread);
+			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,4, i+1);
+			system(findCommand);
 
 			//Placeholder Sleep Tactic 0
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
@@ -149,14 +209,16 @@ void *printMsg(void *ptr)
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
 			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,5, i+1);
-			system("rm 'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+			system(rmCommand);
 
 			//Placeholder Sleep Tactic 0
 			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
 			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
 			usleep(delay);
-	
+
+			strcpy(wgetCommand,"wget ");
+			strcpy(link,"");
 		}
 		
 		sleep(30);
