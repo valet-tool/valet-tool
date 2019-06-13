347499a8beabcf2bc8d4a0ccdbb9d1d4b52c2e94
Parth Sane <pvs3325@rit.edu>
2019-06-13 17:10:04 -0400

    Added changes to cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,8 @@
 killall PiMonitor
 sleep 5
 cd /home/pi/valet-tool
-mv output.csv `date +%Y-%m-%d`-output.csv
 sleep 5
-/home/pi/valet-tool/PiMonitor | tee /home/pi/valet-tool/output.csv
+python3 transformCSVtoTable.py
+sh /home/pi/valet-tool/git.sh
+sleep 5
+/home/pi/valet-tool/PiMonitor
