bcc9f2e6ae11303eba2f07770a9b9e6d23ce96fb
Parth Sane <pvs3325@rit.edu>
2019-06-13 17:04:29 -0400

    Revert "Auto stash before rebase of "origin/master""
    This reverts commit 292b615223b1f07ff60bc6dcc3b08c5a968298a2.

--- cron.sh
+++ cron.sh
@@ -1,8 +1,6 @@
 killall PiMonitor
 sleep 5
 cd /home/pi/valet-tool
+mv output.csv `date +%Y-%m-%d`-output.csv
 sleep 5
-python3 transformCSVtoTable.py
-sh /home/pi/valet-tool/git.sh
-sleep 5
-/home/pi/valet-tool/PiMonitor
+/home/pi/valet-tool/PiMonitor | tee /home/pi/valet-tool/output.csv
