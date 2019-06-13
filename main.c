292b615223b1f07ff60bc6dcc3b08c5a968298a2
Parth Sane <pvs3325@rit.edu>
2019-06-13 16:19:13 -0400

    Auto stash before rebase of "origin/master"

--- main.c
+++ main.c
@@ -45,11 +45,15 @@ int main()
 	pthread_t thread;
 	FILE *file;
 	bool file_exists = false;
-	const char *filename = "output.csv";
+	const char *filename = "output-";
 	printf("Date,Current(mA),Power(mW),Is_Active,Tactic\n");
+	memset(current_date_time, 0x00, sizeof(current_date_time) / sizeof(current_date_time[0]));
+	get_current_date_time(current_date_time);
+	strcat(filename, current_date_time);
+	strcat(filename, ".csv");
 	int iret = pthread_create(&thread, NULL, printMsg, (void *)message1);
 	file = fopen(filename, "r");
-	float current = 0,power = 0;
+	float current = 0, power = 0;
 	if (file != NULL)
 	{
 		file_exists = true;
@@ -66,7 +70,6 @@ int main()
 	while (keep_running)
 	{
 		usleep(delay);
-		memset(current_date_time, 0x00, sizeof(current_date_time) / sizeof(current_date_time[0]));
 		get_current_date_time(current_date_time);
 		current = get_current_in_mA(i2c_bus_1_fd);
 		power = get_power_in_mW(i2c_bus_1_fd);
@@ -81,21 +84,27 @@ int main()
 void *printMsg(void *ptr)
 {
 	FILE *file_thread;
-	const char *filename = "downstamps.csv";
+	const char *filename = "downstamps-";
 	bool file_exists = false;
 	int serverNumber = 0;
 	char current_date_time_thread[26];
 	uint32_t delay = 1000000;
-	char zipfilename[100]="'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'";
-	char debDir[100]= "'LibreOffice_6.2.3_Linux_x86_deb'";
+	char zipfilename[100] = "'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'";
+	char debDir[100] = "'LibreOffice_6.2.3_Linux_x86_deb'";
 	char whatToFind[100] = "'README_en-US'";
-	char filenameArgument[1000]=" '-O' 'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'";
-	char wgetCommand[1000]="wget ";
-    char link[1000]="";
-	char unzipCommand[100]="tar 'xvf' ";
-	char zipCommand[100]="tar '-cvzf' ";
-	char rmCommand[100]="rm ";
-	char findCommand[100]="find './' '-name' ";
+	char filenameArgument[1000] = " '-O' 'LibreOffice_6.2.3_Linux_x86_deb.tar.gz'";
+	char wgetCommand[1000] = "wget ";
+	char link[1000] = "";
+	char unzipCommand[100] = "tar '-xf' ";
+	char zipCommand[100] = "tar '-czf' ";
+	char rmCommand[100] = "rm ";
+	char findCommand[100] = "find './' '-name' ";
+
+	memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
+	get_current_date_time(current_date_time_thread);
+	strcat(filename, current_date_time_thread);
+	strcat(filename, ".csv");
+
 	file_thread = fopen(filename, "r");
 	if (file_thread != NULL)
 	{
@@ -112,115 +121,105 @@ void *printMsg(void *ptr)
 	}
 
 	signal(SIGINT, sig_handler);
-	strcat(findCommand,whatToFind);
-	strcat(rmCommand,zipfilename);
-	strcat(unzipCommand,zipfilename);
-	strcat(zipCommand,zipfilename);
-	strcat(zipCommand,debDir);
+	strcat(findCommand, whatToFind);
+	strcat(rmCommand, zipfilename);
+	strcat(unzipCommand, zipfilename);
+	strcat(zipCommand, zipfilename);
+	strcat(zipCommand, debDir);
 	while (keep_running)
 	{
 		int i = 0;
 		for (i = 0; i < 9; i++)
-		{	
+		{
 			switch (i)
 			{
 			case 0:
-				strcat(link,"'http://tdf.ufes.br/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'http://tdf.ufes.br/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 1:
-				strcat(link,"'http://tdf.mirror.rafal.ca/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'http://tdf.mirror.rafal.ca/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 2:
-				strcat(link,"'https://espejito.fder.edu.uy/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'https://espejito.fder.edu.uy/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 3:
-				strcat(link,"'https://mirror.aptus.co.tz/pub/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'https://mirror.aptus.co.tz/pub/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 4:
-				strcat(link,"'http://ftp.kaist.ac.kr/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'http://ftp.kaist.ac.kr/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 5:
-				strcat(link,"'https://libreoffice.mirror.garr.it/mirrors/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'https://libreoffice.mirror.garr.it/mirrors/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 6:
-				strcat(link,"'http://ftp.bme.hu/pub/mirrors/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'http://ftp.bme.hu/pub/mirrors/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 7:
-				strcat(link,"'https://mirrors.ucr.ac.cr/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'https://mirrors.ucr.ac.cr/tdf/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			case 8:
-				strcat(link,"'https://ftp.nluug.nl/office/libreoffice/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
+				strcat(link, "'https://ftp.nluug.nl/office/libreoffice/libreoffice/stable/6.2.3/deb/x86/LibreOffice_6.2.3_Linux_x86_deb.tar.gz'");
 				break;
 			default:
 				printf("Invalid link: Unkown Error Occured!");
 				exit(1);
 				break;
 			}
-			strcat(wgetCommand,strcat(link,filenameArgument));
+			strcat(wgetCommand, strcat(link, filenameArgument));
 			//Download: tactic 1
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,1, i+1);
+			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread, 1, i + 1);
 			system(wgetCommand);
-			
+
 			//Placeholder Sleep Tactic 0
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
+			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread, '0', i + 1);
 			usleep(delay);
 
 			//Unzip: Tactic 2
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,2, i+1);
+			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread, 2, i + 1);
 			system(unzipCommand);
 
 			//Placeholder Sleep Tactic 0
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
+			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread, '0', i + 1);
 			usleep(delay);
 
 			//find command: Tactic 3
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,3, i+1);
+			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread, 3, i + 1);
 			system(findCommand);
 
 			//Placeholder Sleep Tactic 0
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
+			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread, '0', i + 1);
 			usleep(delay);
 
 			//compress command: Tactic 4
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,4, i+1);
+			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread, 4, i + 1);
 			system(findCommand);
 
 			//Placeholder Sleep Tactic 0
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
+			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread, '0', i + 1);
 			usleep(delay);
 
 			// Delete: Tactic 5
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread,5, i+1);
+			fprintf(file_thread, "%s,%d,%d\n", current_date_time_thread, 5, i + 1);
 			system(rmCommand);
 
 			//Placeholder Sleep Tactic 0
-			memset(current_date_time_thread, 0x00, sizeof(current_date_time_thread) / sizeof(current_date_time_thread[0]));
 			get_current_date_time(current_date_time_thread);
-			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread,'0', i+1);
+			fprintf(file_thread, "%s,%c,%d\n", current_date_time_thread, '0', i + 1);
 			usleep(delay);
 
-			strcpy(wgetCommand,"wget ");
-			strcpy(link,"");
+			strcpy(wgetCommand, "wget ");
+			strcpy(link, "");
 		}
-		
+
 		sleep(30);
 	}
 	fclose(file_thread);
