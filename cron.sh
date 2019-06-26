146d2cd643e8fafb8fe998ad7b9105d1215089e5
rit-valet <services+valet@cioffitech.com>
2019-06-26 16:44:43 -0400

    updated cron script

--- cron.sh
+++ cron.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 tmux kill-session -t 'pimon'
 cd /home/pi/valet-tool
-python3 transformCSVtoTable.py
 sh /home/pi/valet-tool/git.sh
 tmux new-session -d -s 'pimon'
-tmux send -t pimon.0 sh run.sh ENTER
+tmux send -t pimon.0 ./run.sh ENTER
