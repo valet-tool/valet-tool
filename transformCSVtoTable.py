08b55c419f6d2e1e552abfa334d7f917a7930d1d
Parth Sane <laerdevstudios@gmail.com>
2019-12-12 23:28:50 -0500

    Optimised via index to avoid iterating over and over again

--- transformCSVtoTable.py
+++ transformCSVtoTable.py
@@ -1,31 +1,52 @@
 import csv
-import dateutil.parser
 import datetime
+
+import dateutil.parser
 import pandas
 
 TODAY = str(datetime.date.today())
+
+
 def calculate_final_data(timegroups):
     time_records_list = []
     totalPowerForTactic = 0
     with open('rawfiles/output-' + TODAY + '.csv') as time_file:
         time_reader = list(csv.reader(time_file, delimiter=','))
+        counter = 0
         for timegroup in timegroups:
-            startedTimestamp = dateutil.parser.parse(timegroup[0])
-            endedTimestamp = dateutil.parser.parse(timegroup[1])
-            if endedTimestamp <= startedTimestamp:
+            try:
+                startedTimestamp, endedTimestamp = '', ''
+                if timegroup[0] != '' and timegroup[1] != '':
+                    startedTimestamp = dateutil.parser.parse(timegroup[0])
+                    endedTimestamp = dateutil.parser.parse(timegroup[1])
+
+                    if endedTimestamp <= startedTimestamp:
+                        continue
+                    while dateutil.parser.parse(time_reader[counter][0]) <= endedTimestamp:
+                        if startedTimestamp <= dateutil.parser.parse(time_reader[counter][0]) <= endedTimestamp:
+                            totalPowerForTactic += float(time_reader[counter][2])
+                        elif dateutil.parser.parse(time_reader[counter][0]) > endedTimestamp:
+                            break
+                        counter += 1
+                    latency_in_mins = endedTimestamp - startedTimestamp
+                    latency = str(latency_in_mins.total_seconds())
+                    servernum = timegroup[2]
+                    tacticnum = timegroup[3]
+                    record = [totalPowerForTactic, str(startedTimestamp), latency, servernum, tacticnum]
+                    time_records_list.append(record)
+                    totalPowerForTactic = 0
+
+            except IndexError:
                 continue
-            for time_row in time_reader:
-                if startedTimestamp <= dateutil.parser.parse(time_row[0]) <= endedTimestamp:
-                    totalPowerForTactic += float(time_row[2])
-            latency_in_mins = endedTimestamp - startedTimestamp
-            latency = str(latency_in_mins.total_seconds())
-            servernum = timegroup[2]
-            tacticnum = timegroup[3]
-            record = [totalPowerForTactic, str(startedTimestamp), latency, servernum, tacticnum]
-            time_records_list.append(record)
-            totalPowerForTactic = 0
+            # except ValueError:
+            #     continue
+            except UnicodeDecodeError:
+                print('Unicode Error')
+                continue
 
     return time_records_list
+
+
 def create_time_groups():
     with open('rawfiles/downstamps-'+TODAY+'.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -59,6 +80,7 @@ def create_time_groups():
         except UnicodeDecodeError:
             print('Unicode Error')
     return records
+
 
 if __name__ == '__main__':
     timegroups = create_time_groups()
