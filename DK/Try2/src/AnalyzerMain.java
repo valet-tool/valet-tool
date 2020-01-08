8c86e87f2451969ca8eb8f63aae113e123b629da
Dan <dan7800@yahoo.com>
2020-01-08 15:24:16 -0500

    temp checkin - problems found with raw data

--- DK/Try2/src/AnalyzerMain.java
+++ DK/Try2/src/AnalyzerMain.java
@@ -15,6 +15,13 @@ public class AnalyzerMain {
     private final String DbLocation = "/Users/dkrutz/Documents/GIT/valet-tool/DK/data/Try1.sqlite";
 
 
+  //  private final String down = "down";
+  //  private final String out = "out";
+
+    private final String down = "down_722";
+    private final String out = "out_722";
+
+
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
@@ -42,7 +49,7 @@ public class AnalyzerMain {
             stmt2 = c.createStatement();
 
            // final String sqlAllDown="select * from down where ID > 2 limit 1;";
-            final String sqlAllDown="select * from down where ID > 2;";
+            final String sqlAllDown="select * from " + down +  " where ID > 2;";
             ResultSet rsAllDown = stmt.executeQuery( sqlAllDown );
             System.out.println(sqlAllDown);
 
@@ -62,7 +69,7 @@ public class AnalyzerMain {
 
 
                     // Get the time when the tactic ended
-                    final String nextTimeSearch="select ID, ActionTime from down where ID = " +  (downID + 1) + ";";
+                    final String nextTimeSearch="select ID, ActionTime from " + down +  " where ID = " +  (downID + 1) + ";";
                     System.out.println(nextTimeSearch);
                     ResultSet rsNextTime = stmt2.executeQuery( nextTimeSearch ); // need to be statement 2 to differentiate it from the other statement
 
@@ -79,7 +86,7 @@ public class AnalyzerMain {
                     stmt2.close();
 
 
-                    String query = "update down set latency = ? where ID = ?";
+                    String query = "update " + down +  " set latency = ? where ID = ?";
                     PreparedStatement preparedStmt = c.prepareStatement(query);
                     preparedStmt.setLong   (1, Latencydiff);
                     preparedStmt.setInt   (2, downID);
@@ -99,7 +106,7 @@ public class AnalyzerMain {
 
 
                     // Get Date Before
-                    final String findBeforeTime="select ID, ActionTime from down where ID = " +  (downID - 1) + ";";
+                    final String findBeforeTime="select ID, ActionTime from " + down + " where ID = " +  (downID - 1) + ";";
                    // System.out.println(findBeforeTime);
                     ResultSet rsBeforeTime = stmt2.executeQuery( findBeforeTime ); // need to be statement 2 to differentiate it from the other statement
 
@@ -120,7 +127,7 @@ public class AnalyzerMain {
 
                     // Now get all the cost values for each of the three slots
 
-                    final String findCostBefore="select ID, ActionTime, Cost1, Cost2 from out where ActionTime >= '"+DateBefore+ "' and ActionTime <= '" + DateStart +"';";
+                    final String findCostBefore="select ID, ActionTime, Cost1, Cost2 from " + out + " where ActionTime >= '"+DateBefore+ "' and ActionTime <= '" + DateStart +"';";
                     System.out.println(findCostBefore);
                     ResultSet rsCostBeforeVals = stmt2.executeQuery( findCostBefore ); // need to be statement 2 to differentiate it from the other statement
 
@@ -153,7 +160,7 @@ public class AnalyzerMain {
 
 
                     // Now Get the Average Cost During
-                    final String findCostDuring="select ID, ActionTime, Cost1, Cost2 from out where ActionTime >= '" +DateStart + "' and ActionTime <= '"+  DateEnd  +"';";
+                    final String findCostDuring="select ID, ActionTime, Cost1, Cost2 from " + out + " where ActionTime >= '" +DateStart + "' and ActionTime <= '"+  DateEnd  +"';";
                     System.out.println(findCostDuring);
                     ResultSet rsCostDuringVals = stmt2.executeQuery( findCostDuring ); // need to be statement 2 to differentiate it from the other statement
 
@@ -188,7 +195,7 @@ public class AnalyzerMain {
 
                     // Start by getting the end tactic time after
 
-                    final String findCostAfterTactic="select ID, ActionTime from down where ID = " +  (downID + 2) + ";";
+                    final String findCostAfterTactic="select ID, ActionTime from " + down + " where ID = " +  (downID + 2) + ";";
                     ResultSet findNextTatcitStartTime = stmt2.executeQuery( findCostAfterTactic ); // need to be statement 2 to differentiate it from the other statement
 
                     final String lastTacticTime =  findNextTatcitStartTime.getString("ActionTime");
@@ -199,7 +206,7 @@ public class AnalyzerMain {
                     stmt2.close();
 
 
-                    final String findCostAfter="select ID, ActionTime, Cost1, Cost2 from out where ActionTime >= '" + DateEnd +   "' and ActionTime <= '"+ lastTacticTime +"';";
+                    final String findCostAfter="select ID, ActionTime, Cost1, Cost2 from " + out + " where ActionTime >= '" + DateEnd +   "' and ActionTime <= '"+ lastTacticTime +"';";
                     System.out.println(findCostAfter);
                     ResultSet rsCostAfterVals = stmt2.executeQuery( findCostAfter ); // need to be statement 2 to differentiate it from the other statement
 
@@ -250,7 +257,7 @@ public class AnalyzerMain {
 
 
                     // Save all of the cost results
-                    String query2 = "update down set Cost1 = ?, Cost2 = ?, Cost1Before = ?, Cost1During = ?, Cost1After = ?, Cost2Before = ?, Cost2During = ?, Cost2After = ? where ID = ?";
+                    String query2 = "update " + down +  " set Cost1 = ?, Cost2 = ?, Cost1Before = ?, Cost1During = ?, Cost1After = ?, Cost2Before = ?, Cost2During = ?, Cost2After = ? where ID = ?";
                     PreparedStatement preparedStmt2 = c.prepareStatement(query2);
                     preparedStmt2.setDouble  (1, Cost1AvgDiff);
                     preparedStmt2.setDouble  (2, Cost2AvgDiff);
